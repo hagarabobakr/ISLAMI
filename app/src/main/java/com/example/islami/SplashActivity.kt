54e200a6e8193db03d27a61fc8b6dec5aca69025
hager <https://github.com/hagarabobakr/islami_hager_abobakr.git>
2023-02-26 15:00:40 +0200

    QURAN SECTION

--- app/src/main/java/com/example/islami/SplashActivity.kt
+++ app/src/main/java/com/example/islami/SplashActivity.kt
@@ -1,19 +1,19 @@
 package com.example.islami
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
         Handler(Looper.getMainLooper()).postDelayed({
-            val intent=Intent(this@SplashActivity,HomeActivity::class.java)
+            val intent = Intent(this@SplashActivity, HomeActivity::class.java)
             startActivity(intent)
-        },2000)
+        }, 2000)
 
 
     }
--- app/src/main/java/com/example/islami/SuraNameAdapter.kt
+++ app/src/main/java/com/example/islami/SuraNameAdapter.kt
@@ -7,27 +7,30 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.Adapter
 
-class SuraNameAdapter(val item:List<String> ):Adapter<SuraNameAdapter.viewHolder>() {
-    class viewHolder(ItemView: View):RecyclerView.ViewHolder(ItemView){
-        val SuraName=ItemView.findViewById<TextView>(R.id.item_sura_name)
+class SuraNameAdapter(val item: List<String>) : Adapter<SuraNameAdapter.viewHolder>() {
+    class viewHolder(ItemView: View) : RecyclerView.ViewHolder(ItemView) {
+        val SuraName = ItemView.findViewById<TextView>(R.id.item_sura_name)
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): viewHolder {
-       val view=LayoutInflater.from(parent.context).inflate(R.layout.item_sura_name,parent,false)
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.item_sura_name, parent, false)
         return viewHolder(view)
     }
 
     override fun onBindViewHolder(holder: viewHolder, position: Int) {
-        holder.SuraName.text= item[position]
+        holder.SuraName.text = item[position]
         holder.itemView.setOnClickListener {
-            on_item_click?.onSuraNameClick(item.get(position),position)
+            on_item_click?.onSuraNameClick(item.get(position), position)
         }
     }
-    interface onItemClick{
-        fun onSuraNameClick(sura:String,position: Int)
+
+    interface onItemClick {
+        fun onSuraNameClick(sura: String, position: Int)
     }
-    var on_item_click:onItemClick ? =null
+
+    var on_item_click: onItemClick? = null
 
     override fun getItemCount(): Int {
         return item.size
